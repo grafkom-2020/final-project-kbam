c222fe19d418f81f75c32b6d21d2a59d41086bf6
Kresna Adhi Pramana <57925077+KresnaAP@users.noreply.github.com>
2020-05-14 21:13:35 +0700

    Create clouds and cloud rotation
    - Create clouds
    - Add clouds rotation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var scene, camera, fov, ratio, near, far, renderer, container,
     width, height,
-    background, ground;
+    background, ground, cloudy;
 
 var skyIndex = 0, skyIncrease = 1, skyTime = 0;
 var skyColor = [
@@ -67,6 +67,56 @@ function createGround(){
     scene.add(ground);
 }
 
+// Buat Awan
+function cloneCloud(){
+    this.mesh = new THREE.Object3D();
+    this.nClouds = 40; 
+    this.clouds = [];
+    // Buat angle awan
+    var stepAngle = Math.PI*2 / this.nClouds;
+    for(var i=0; i<this.nClouds; i++){
+        var c = new Cloud();
+        this.clouds.push(c);
+        var a = stepAngle*i;
+        var h = 750 + Math.random()*200;
+        c.mesh.position.y = Math.sin(a)*h;
+        c.mesh.position.x = Math.cos(a)*h;
+        c.mesh.position.z = -400-Math.random()*400;
+        c.mesh.rotation.z = a + Math.PI/2;
+        var s = 1+Math.random()*2;
+        c.mesh.scale.set(s,s,s);
+        this.mesh.add(c.mesh);
+    }
+}
+  
+function Cloud(){
+    this.mesh = new THREE.Object3D();
+    var geom = new THREE.SphereGeometry(15,15,15);
+    var mat = new THREE.MeshBasicMaterial({
+        color: 0xF0F0F0,
+    });
+    
+    // Buat ukuran
+    var nBlocs = 3+Math.floor(Math.random()*3);
+    for (var i=0; i<nBlocs; i++ ){
+        var m = new THREE.Mesh(geom.clone(), mat);
+        m.position.x = i*15;
+        m.position.y = Math.random()*10;
+        m.position.z = Math.random()*10;
+        m.rotation.z = Math.random()*Math.PI*2;
+        m.rotation.y = Math.random()*Math.PI*2;
+        var s = .1 + Math.random()*.9;
+        m.scale.set(s,s,s);
+        this.mesh.add(m);
+    }
+}
+
+function createCloud(){
+    cloudy = new cloneCloud();
+    cloudy.mesh.position.y = -500;
+    scene.add(cloudy.mesh);
+}
+
 // Animasi
 function loop(){
     renderer.render(scene, camera);
@@ -87,7 +137,8 @@ function loop(){
     }
     // Animate Ground
     ground.rotation.z += 0.005;
-
+    // Animate Cloud
+    cloudy.mesh.rotation.z += .01;
       
     requestAnimationFrame(loop);
 }
@@ -97,6 +148,7 @@ function init(){
     createScene();
     createBackground();
     createGround();
+    createCloud();
     loop();
 }
 
